18792be524c59f0c00ff7351763e354d3f2669d3
Wu Huijie <wu_huijie@tech.gov.sg>
2024-10-28 01:52:07 +0800

    add functionality to expand on facts

--- app/pages/2_Sustainability_Facts.py
+++ app/pages/2_Sustainability_Facts.py
@@ -3,31 +3,57 @@ import page
 import env
 from services import policies
 
+FACT_SEPARATOR = '---sep---'
+
+def sanitise_reply(text):
+    return text.replace('<Response>', '').replace('</Response>', '')
+
 class SustainabilityFacts(page.PageTemplate):
     def render(self):
         if super().render():
             st.button(label='Refresh')
 
             policy_svc = policies.PolicyService(debug_mode=env.is_debug_mode())
-            reply = policy_svc.query_selected_policy("""
+            reply = policy_svc.query_all_policies(f"""
 Assuming you are a teacher familiar with all environmental-related topics. \
 List down 3 facts related to all policies in an interesting manner as if you are teaching a class of students. \
-Present the facts according to following markdown text enclosed in <Facts> tag:
+Present the facts according to following JSON format enclosed in <Response> tag:
 
-<Facts>
+<Response>
 ### Title
 
 Content about fact 1
 
+{FACT_SEPARATOR}
+
 ### Title
 
 Content about fact 2
-</Facts>
+</Response>
 
-Each fact should not exceed 200 words and each header should start with an appropriate emoji. Your reply should contain markdown text only and have 3 facts and exclude triple backticks or any enclosing <Facts> tags.
+Each fact should not exceed 200 words and each header should start with an appropriate emoji. \
+Each fact section should be separated by "{FACT_SEPARATOR}" \
+Your reply should contain markdown text only and have 3 facts and exclude triple backticks or any enclosing <Response> tags.
 """)
-            st.markdown(reply)
+            
+            sections = sanitise_reply(reply).split(FACT_SEPARATOR)
+            
+            for idx, s in enumerate(sections):
+                with st.container(border=True):
+                    st.markdown(s)
+                    if st.button(label='Tell Me More', key=idx):
+                        reply2 = policy_svc.query_all_policies(f"""
+Assuming you are a teacher familiar with all environmental-related topics. \
+Elaborate more on content enclosed in <Content> tag in an interesting manner.
+
+<Content>
+{s}
+</Content>
+""")
+                        st.markdown(reply2)
+            
             return True
+        
         return False
 
 sustainabilityFacts = SustainabilityFacts('Sustainability Facts')
